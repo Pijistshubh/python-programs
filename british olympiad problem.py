15cdbb1e815e4d619ce1a0ea9070dd7410e32ce2
pijistSJ <70806500+pijistSJ@users.noreply.github.com>
2020-09-26 16:08:35 +0530

    Update and rename british olympiad problem to british olympiad problem.py

--- british olympiad problem.py
+++ british olympiad problem.py
@@ -1,11 +1,10 @@
 #problem is statement of this link: https://youtu.be/0Hl75Pap6iY
 
-
+#----------------------------------------------------------------------
 """ first code : with branchless programming and list comprihension (lil bit advance than basic python) """
 
 
 import math 
-lst=[]
 ls=[i*(math.sqrt(i)-math.floor(math.sqrt(i))==0) for i in range(99_999, 100_00_00)]
 
 r=list(filter(lambda x: x!=0,ls))
@@ -16,9 +15,8 @@ main = [i*(int(str(i)[:3])+1==int(str(i)[3:])) for i in rl]
 fin=r=list(filter(lambda x: x!=0,main))
 print(fin)
 
-
+#--------------------------------------------------------------------------------
 """ second code : same code with no branchless programming and list comprehension"""
-
 
 import math 
 ls=[]
